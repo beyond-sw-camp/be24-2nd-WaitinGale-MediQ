84624d25417ab81bb2d89715b3d865614b4f1cfa
mihub02 <jay06holi@gmail.com>
2026-01-20 10:11:01 +0900

    [Rename] - 메인 파일 경로 변경

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -8,37 +8,31 @@ const router = createRouter({
       name: 'home',
       path: '/', // 앞에 슬래시(/) 붙여서 독립 경로로 만듦
       meta: { title: 'home' },
-      component: () => import('../views/user/HomeView.vue'), // 레이지 로딩 유지
+      component: () => import('@/views/user/HomeView.vue'), // 레이지 로딩 유지
     },
     {
       name: 'login',
       path: '/login', 
       meta: { title: '로그인' },
-      component: () => import('../views/user/UserLogin.vue'), // 레이지 로딩 유지
+      component: () => import('@/views/user/Login.vue'), // 레이지 로딩 유지
     },
     {
       name: 'UserFindId',
       path: '/UserFindId',
       meta: { title: '아이디 찾기' },
-      component: () => import('../views/user/UserFindId.vue'), // 레이지 로딩 유지
+      component: () => import('@/views/user/FindId.vue'), // 레이지 로딩 유지
     },
     {
       name: 'UserFindPassword',
       path: '/UserFindPassword',
       meta: { title: '비밀번호 찾기' },
-      component: () => import('../views/user/UserFindPassword.vue'), // 레이지 로딩 유지
+      component: () => import('@/views/user/FindPassword.vue'), // 레이지 로딩 유지
     },
     {
       name: 'signup',
       path: '/signup',
       meta: { title: '회원가입' },
-      component: () => import('../views/user/UserSignup.vue'), // 레이지 로딩 유지
-    },
-    {
-      name: 'UserMain',
-      path: '/UserMain',
-      meta: { title: 'UserMain' },
-      component: () => import('../views/UserMain.vue'), // 레이지 로딩 유지
+      component: () => import('@/views/user/Signup.vue'), // 레이지 로딩 유지
     },
 
     {
